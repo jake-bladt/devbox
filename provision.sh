506290b04de773ae1b3c06c70ceebff93bb8d1cd
Jake Bladt <jake@jakebladt.com>
2016-12-21 20:51:03 -0500

    +pip & virtualenv

--- provision.sh
+++ provision.sh
@@ -2,5 +2,7 @@
 
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 sudo apt-get update
-sudo apt-get install build-essential libssl-dev gettext unzip git -y
+sudo apt-get install build-essential libssl-dev gettext unzip git python-pip  python-dev -y
 \curl -sSL https://get.rvm.io | bash -s stable --rails
+sudo pip install virtualenv virtualenvwrapper
+sudo pip install --upgrade pip
