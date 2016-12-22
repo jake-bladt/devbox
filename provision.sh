37955f1a1debdea14b751203c4c2ffda7f5f4f7c
Jake Bladt <jake@jakebladt.com>
2016-12-21 20:34:34 -0500

    +rvm/ruby/rails

--- provision.sh
+++ provision.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 sudo apt-get update
 sudo apt-get install build-essential libssl-dev gettext unzip git -y
+\curl -sSL https://get.rvm.io | bash -s stable --rails
