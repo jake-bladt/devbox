4a028b18402cb95a42babef74a7d5aef6ffe883f
Jake Bladt <jake@jakebladt.com>
2016-12-21 21:09:41 -0500

    +docker

--- provision.sh
+++ provision.sh
@@ -2,7 +2,8 @@
 
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 sudo apt-get update
-sudo apt-get install build-essential libssl-dev gettext unzip git python-pip  python-dev -y
+sudo apt-get install build-essential libssl-dev gettext unzip git python-pip  python-dev -y \
+   apt-transport-https ca-certificates
 \curl -sSL https://get.rvm.io | bash -s stable --rails
 sudo pip install virtualenv virtualenvwrapper
 sudo pip install --upgrade pip
@@ -10,3 +11,17 @@ sudo pip install --upgrade pip
 printf '\n%s\n%s\n%s' '# virtualenv' 'export WORKON_HOME=~/virtualenvs' \
 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
 source ~/.bashrc
+
+sudo apt-key adv \
+               --keyserver hkp://ha.pool.sks-keyservers.net:80 \
+               --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+apt-cache policy docker-engine
+sudo apt-get install docker-engine
+
+
+
+
+sudo service docker start
